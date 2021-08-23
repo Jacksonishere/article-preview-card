2bd7d848a65183f193ab152f69c5f85222b02bd2
jackson.lu02@myhunter.cuny.edu <jackson.lu02@myhunter.cuny.edu>
2021-08-23 18:22:44 -0400

    animation to mobile sharebtn added

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 let toggle = null;
 
 const button = document.getElementById("share-btn");
+
 const showShare = document.getElementById("hidden-share");
 
-button.addEventListener("click", (e) =>{
-    if(toggle === null){
-        toggle = true;
-        showShare.classList.add("in");
-    }
-    else{
-        if(toggle === true){
-            showShare.classList.remove("in");
-        }
-        else{
-            showShare.classList.add("in");
-        }
-        toggle = !toggle;
-    }
-})
+button.addEventListener("click", (e) => {
+	if (toggle === null) {
+		toggle = true;
+		showShare.classList.add("in");
+        button.classList.add("in-btn");
+	} else {
+		if (toggle === true) {
+			showShare.classList.remove("in");
+            button.classList.remove("in-btn");
+		} else {
+			showShare.classList.add("in");
+            button.classList.add("in-btn");
+		}
+		toggle = !toggle;
+	}
+});
 
 console.log("comprehendos?");
-
-
